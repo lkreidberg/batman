63254b952223993e04b346476463c5bd2e8e9dc7
Ethan Kruse <eakruse@uw.edu>
2016-03-07 21:00:12 -0800

    Add in the OPENMP bits, and now quadratic LD is more accurate.

--- c_src/_quadratic_ld.c
+++ c_src/_quadratic_ld.c
@@ -50,7 +50,7 @@ static PyObject *_quadratic_ld(PyObject *self, PyObject *args)
 */
     int nd, nthreads;
     double c1, c2, p, lambdad, etad, \
-        lambdae, lam, x1, x2, x3, d, omega, kap0 = 0.0, kap1 = 0.0, \
+        lambdae, x1, x2, x3, d, omega, kap0 = 0.0, kap1 = 0.0, \
         q, Kk, Ek, Pk, n;
     PyArrayObject *ds, *flux;
     npy_intp i, dims[1];
@@ -75,20 +75,41 @@ static PyObject *_quadratic_ld(PyObject *self, PyObject *args)
         Laura Kreidberg 07/2015
     */
 
-    if(fabs(p - 0.5) < 1.0e-3) p = 0.5;
-
     omega = 1.0 - c1/3.0 - c2/6.0;
+    double tol = 1.0e-14;
 
     #if defined (_OPENMP)
     omp_set_num_threads(nthreads);
     #endif
 
     #if defined (_OPENMP)
-    #pragma omp parallel for private(d, x1, x2, x3, n, q, Kk, Ek, Pk, kap0, kap1)
+    #pragma omp parallel for private(d, x1, x2, x3, n, q, Kk, Ek, Pk, kap0, kap1, lambdad, lambdae, etad)
     #endif
     for(i = 0; i < dims[0]; i++)
     {
         d = d_array[i];         // separation of centers
+
+        // allow for negative impact parameters
+        d = fabs(d);
+
+        // check the corner cases
+        if(fabs(p - d) < tol)
+        {
+            d = p;
+        }
+        if(fabs(p-1.0-d) < tol)
+        {
+            d = p-1.0;
+        }
+        if(fabs(1.0-p-d) < tol)
+        {
+            d = 1.0-p;
+        }
+        if(d < tol)
+        {
+            d = 0.0;
+        }
+
         x1 = pow((p - d), 2.0);
         x2 = pow((p + d), 2.0);
         x3 = p*p - d*d;
@@ -97,11 +118,7 @@ static PyObject *_quadratic_ld(PyObject *self, PyObject *args)
         if(d >= 1.0 + p)
         {
             //printf("zone 1\n");
-            lambdad = 0.0;
-            etad = 0.0;
-            lambdae = 0.0;
-            f_array[i] = 1.0 - ((1.0 - c1 - 2.0*c2)*lambdae + (c1 + 2.0*c2)*lambdad + c2*etad)/omega;
-
+            f_array[i] = 1.0;
             continue;
         }
         //source is completely occulted:
@@ -123,29 +140,25 @@ static PyObject *_quadratic_ld(PyObject *self, PyObject *args)
             lambdae = p*p*kap0 + kap1;
             lambdae = (lambdae - 0.50*sqrt(MAX(4.0*d*d - pow((1.0 + d*d - p*p), 2.0), 0.0)))/M_PI;
         }
-        //occulting object transits the source but doesn't completely cover it:
-        if(d <= 1.0 - p)
-        {
-            //printf("zone 4\n");
-            lambdae = p*p;
-        }
+
         //edge of the occulting star lies at the origin
-        if(fabs(d - p) < 1.0e-4*(d + p))
+        if(d == p)
         {
-            d = p;
             //printf("zone 5\n");
-            if(p == 0.5)
+            if(d<0.5)
             {
-                //printf("zone 6\n");
-                lambdad = 1.0/3.0 - 4.0/M_PI/9.0;
-                etad = 3.0/32.0;
+                //printf("zone 5.2\n");
+                q = 2.0*p;
+                Kk = ellk(q);
+                Ek = ellec(q);
+                lambdad = 1.0/3.0 + 2.0/9.0/M_PI*(4.0*(2.0*p*p - 1.0)*Ek + (1.0 - 4.0*p*p)*Kk);
+                etad = p*p/2.0*(p*p + 2.0*d*d);
                 f_array[i] = 1.0 - ((1.0 - c1 - 2.0*c2)*lambdae + (c1 + 2.0*c2)*lambdad + c2*etad)/omega;
                 continue;
             }
-            else if(d >= 0.5)
+            else if(d > 0.5)
             {
                 //printf("zone 5.1\n");
-                lam = 0.5*M_PI;
                 q = 0.5/p;
                 Kk = ellk(q);
                 Ek = ellec(q);
@@ -155,18 +168,15 @@ static PyObject *_quadratic_ld(PyObject *self, PyObject *args)
                                   (1.0 + 5.0*p*p + d*d)/4.0*sqrt((1.0 - x1)*(x2 - 1.0)));
             //    continue;
             }
-            else if(d<0.5)
+            else
             {
-                //printf("zone 5.2\n");
-                lam = 0.50*M_PI;
-                q = 2.0*p;
-                Kk = ellk(q);
-                Ek = ellec(q);
-                lambdad = 1.0/3.0 + 2.0/9.0/M_PI*(4.0*(2.0*p*p - 1.0)*Ek + (1.0 - 4.0*p*p)*Kk);
-                etad = p*p/2.0*(p*p + 2.0*d*d);
+                //printf("zone 6\n");
+                lambdad = 1.0/3.0 - 4.0/M_PI/9.0;
+                etad = 3.0/32.0;
                 f_array[i] = 1.0 - ((1.0 - c1 - 2.0*c2)*lambdae + (c1 + 2.0*c2)*lambdad + c2*etad)/omega;
                 continue;
             }
+
             f_array[i] = 1.0 - ((1.0 - c1 - 2.0*c2)*lambdae + (c1 + 2.0*c2)*lambdad + c2*etad)/omega;
             continue;
         }
@@ -177,8 +187,7 @@ static PyObject *_quadratic_ld(PyObject *self, PyObject *args)
             && d < p))
         {
             //printf("zone 3.1\n");
-            lam = 0.50*M_PI;
-            q = sqrt((1.0 - pow((p - d), 2.0))/4.0/d/p);
+            q = sqrt((1.0 - x1)/4.0/d/p);
             Kk = ellk(q);
             Ek = ellec(q);
             n = 1.0/x1 - 1.0;
@@ -193,10 +202,12 @@ static PyObject *_quadratic_ld(PyObject *self, PyObject *args)
             continue;
         }
         //occulting star transits the source:
-        if(p <= 1.0  && d <= (1.0 - p)*1.0001)
+        if(p <= 1.0  && d <= (1.0 - p))
         {
+            etad = p*p/2.0*(p*p + 2.0*d*d);
+            lambdae = p*p;
+
             //printf("zone 4.1\n");
-            lam = 0.50*M_PI;
             q = sqrt((x2 - x1)/(1.0 - x1));
             Kk = ellk(q);
             Ek = ellec(q);
@@ -205,13 +216,14 @@ static PyObject *_quadratic_ld(PyObject *self, PyObject *args)
 
             lambdad = 2.0/9.0/M_PI/sqrt(1.0 - x1)*((1.0 - 5.0*d*d + p*p +  \
                      x3*x3)*Kk + (1.0 - x1)*(d*d + 7.0*p*p - 4.0)*Ek - 3.0*x3/x1*Pk);
-            if(d < p) lambdad += 2.0/3.0;
-            if(fabs(p + d - 1.0) <= 1.0e-4)
+
+            // edge of planet hits edge of star
+            if(fabs(p + d - 1.0) <= tol)
             {
                 lambdad = 2.0/3.0/M_PI*acos(1.0 - 2.0*p) - 4.0/9.0/M_PI* \
                             sqrt(p*(1.0 - p))*(3.0 + 2.0*p - 8.0*p*p);
             }
-            etad = p*p/2.0*(p*p + 2.0*d*d);
+            if(d < p) lambdad += 2.0/3.0;
         }
         f_array[i] = 1.0 - ((1.0 - c1 - 2.0*c2)*lambdae + (c1 + 2.0*c2)*lambdad + c2*etad)/omega;
     }
@@ -279,7 +291,7 @@ double ellec(double k)
 {
     double m1, a1, a2, a3, a4, b1, b2, b3, b4, ee1, ee2, ellec;
     // Computes polynomial approximation for the complete elliptic
-    // integral of the second kind (Hasting's approximation):
+    // integral of the first kind (Hasting's approximation):
     m1 = 1.0 - k*k;
     a1 = 0.44325141463;
     a2 = 0.06260601220;
@@ -299,7 +311,7 @@ double ellk(double k)
 {
     double a0, a1, a2, a3, a4, b0, b1, b2, b3, b4, ellk,  ek1, ek2, m1;
     // Computes polynomial approximation for the complete elliptic
-    // integral of the first kind (Hasting's approximation):
+    // integral of the second kind (Hasting's approximation):
     m1 = 1.0 - k*k;
     a0 = 1.38629436112;
     a1 = 0.09666344259;
