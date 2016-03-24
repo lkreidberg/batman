18c7d352872179b482c99b51ab40455142a30dab
Laura Kreidberg <vivacelaura@gmail.com>
2016-03-24 18:01:22 -0500

    add def of MPI

--- c_src/_rsky.c
+++ c_src/_rsky.c
@@ -20,6 +20,10 @@
 #include "numpy/arrayobject.h"
 #include<math.h>
 
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
 static PyObject *_rsky(PyObject *self, PyObject *args);
 
 static PyObject *_getf(PyObject *self, PyObject *args);
